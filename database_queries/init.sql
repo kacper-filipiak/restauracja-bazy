43391c5a07f01d652563c948873b153a800eeb65
Kacper Filipiak <kisielWrosole@gmail.com>
2023-11-19 15:24:23 +0100

    Fixed types of columns and added insert procedures for danie

--- database_queries/init.sql
+++ database_queries/init.sql
@@ -1,77 +1,66 @@
 CREATE DATABASE IF NOT EXISTS restauracja_db;
 USE restauracja_db;
 
-CREATE TABLE IF NOT EXISTS menu (
-  id_dania int(10) NOT NULL AUTO_INCREMENT, 
-  cena     int(10), 
-  PRIMARY KEY (id_dania));
-
-CREATE TABLE IF NOT EXISTS role (
+CREATE TABLE IF NOT EXISTS  role (
   id_roli int(10) NOT NULL AUTO_INCREMENT, 
-  nazwa   varchar(256), 
- PRIMARY KEY (id_roli));
- 
+  nazwa   varchar(256) UNIQUE, 
+  PRIMARY KEY (id_roli));
+CREATE TABLE IF NOT EXISTS uzytkownicy (
+  id_uzytkownika int(10) NOT NULL AUTO_INCREMENT, 
+  login          varchar(256) UNIQUE, 
+  haslo          varchar(256), 
+  rola           int(10), 
+  id_roli int(10) NOT NULL, 
+  PRIMARY KEY (id_uzytkownika),
+  FOREIGN KEY (id_roli) REFERENCES role (id_roli));
 CREATE TABLE IF NOT EXISTS stoliki (
   id_stolika     int(10) NOT NULL AUTO_INCREMENT, 
   status_stolika int(2), 
   PRIMARY KEY (id_stolika));
-
-CREATE TABLE IF NOT EXISTS definicje_produktów (
-  id_definicja int(10) NOT NULL AUTO_INCREMENT, 
-  jednostka    varchar(10), 
-  nazwa        varchar(256), 
-  PRIMARY KEY (id_definicja));
-
-
-CREATE TABLE IF NOT EXISTS uzytkownicy (
-  id_uzytkownika int(10) NOT NULL AUTO_INCREMENT, 
-  login          varchar(256), 
-  haslo          varchar(256), 
-  id_roli int(10) NOT NULL ,
-  FOREIGN KEY (id_roli) REFERENCES role (id_roli), 
-  PRIMARY KEY (id_uzytkownika) 
-  );
-
 CREATE TABLE IF NOT EXISTS zamowienia (
   id_zamowienia     int(10) NOT NULL AUTO_INCREMENT, 
+  id_stolika        int(10), 
   status_zamowienia int(3), 
   data_zamowienia   int(10), 
-  id_stolika           int(10) NOT NULL, 
-  FOREIGN KEY (id_stolika) REFERENCES stoliki (id_stolika), 
-  PRIMARY KEY (id_zamowienia) 
-  );
-
+  stoliki           int(10) NOT NULL, 
+  PRIMARY KEY (id_zamowienia),
+  FOREIGN KEY (stoliki) REFERENCES stoliki (id_stolika));
+CREATE TABLE IF NOT EXISTS  definicje_produktow (
+  id_definicja int(10) NOT NULL AUTO_INCREMENT, 
+  jednostka    varchar(10), 
+  nazwa        varchar(256) UNIQUE, 
+  PRIMARY KEY (id_definicja));
 CREATE TABLE IF NOT EXISTS produkty (
   id_produkt                         int(10) NOT NULL AUTO_INCREMENT, 
   typ                                int(10), 
-  ilosc                              int(10), 
+  ilosc                              float, 
   data_waznosci                      date, 
-  id_definicji_produktu int(10) NOT NULL, 
-  PRIMARY KEY (id_produkt), 
-  FOREIGN KEY (id_definicji_produktu) REFERENCES definicje_produktów (id_definicja));
-
-CREATE TABLE IF NOT EXISTS danie (
+  id_definicja int(10) NOT NULL, 
+  PRIMARY KEY (id_produkt),
+  FOREIGN KEY (id_definicja) REFERENCES definicje_produktow (id_definicja));
+CREATE TABLE IF NOT EXISTS  danie (
   id_dania    int(10) NOT NULL AUTO_INCREMENT, 
-  nazwa_dania varchar(256), 
-  id_zamowienia  int(10) NOT NULL, 
-  id_menu        int(10) NOT NULL, 
-  PRIMARY KEY (id_dania), 
-  FOREIGN KEY (id_menu) REFERENCES menu (id_dania), 
-  FOREIGN KEY (id_zamowienia) REFERENCES zamowienia (id_zamowienia));
+  nazwa_dania varchar(256) UNIQUE, 
+  PRIMARY KEY (id_dania));
+CREATE TABLE IF NOT EXISTS menu (
+  id_dania int(10) NOT NULL, 
+  cena     int(10), 
+  PRIMARY KEY (id_dania),
+  FOREIGN KEY (id_dania) REFERENCES danie (id_dania));
 
-CREATE TABLE IF NOT EXISTS skladniki_dania (
-  id_skladnika_dania              int(10) NOT NULL AUTO_INCREMENT, 
-  zuzywana_ilosc        int(10), 
-  id_definicji_produktu   int(10) NOT NULL, 
-  id_dania                 int(10) NOT NULL, 
-  PRIMARY KEY (id_skladnika_dania),
-  FOREIGN KEY (id_dania) REFERENCES danie (id_dania),
-  FOREIGN KEY (id_definicji_produktu) REFERENCES definicje_produktów (id_definicja));
-  
-DELIMITER $$
-CREATE PROCEDURE IF NOT EXISTS get_menu()
-BEGIN
-  SELECT COUNT(*) FROM menu;
-END $$
-DELIMITER ;
- 
+CREATE TABLE IF NOT EXISTS  skladniki_dania (
+  id_definicji_produktu int(10) NOT NULL, 
+  zuzywana_ilosc        float, 
+  danie_id                 int(10) NOT NULL, 
+  PRIMARY KEY (id_definicji_produktu, 
+  danie_id),
+  FOREIGN KEY (danie_id) REFERENCES danie (id_dania) ON DELETE CASCADE,
+  FOREIGN KEY (id_definicji_produktu) REFERENCES definicje_produktow (id_definicja));
+CREATE TABLE IF NOT EXISTS  zamowienia_danie (
+  zamowienia_id int(10) NOT NULL, 
+  danie_id           int(10) NOT NULL, 
+  PRIMARY KEY (zamowienia_id, 
+  danie_id),
+  FOREIGN KEY (zamowienia_id) REFERENCES zamowienia (id_zamowienia),
+  FOREIGN KEY (danie_id) REFERENCES danie (id_dania));
+
---
+++ database_queries/procedures.sql
@@ -0,0 +1,79 @@
+DELIMITER $$
+ DROP PROCEDURE IF EXISTS get_menu;
+ CREATE PROCEDURE get_menu()
+ BEGIN
+ SELECT cena, nazwa_dania FROM menu INNER JOIN danie ON danie.id_dania = menu.id_dania;
+ END $$
+
+DROP PROCEDURE IF EXISTS insert_definicja_produktu;
+CREATE PROCEDURE insert_definicja_produktu(IN p_jednostka varchar(10), IN p_nazwa varchar(256))
+BEGIN
+INSERT INTO definicje_produktow (jednostka, nazwa) VALUES (p_jednostka, p_nazwa);
+
+END; $$
+
+ DROP PROCEDURE IF EXISTS insert_danie;
+ CREATE PROCEDURE insert_danie(IN p_nazwa varchar(256))
+ BEGIN
+ INSERT INTO danie (nazwa_dania) VALUES (p_nazwa);
+
+ END; $$
+
+ DROP PROCEDURE IF EXISTS get_id_dania_by_nazwa;
+ CREATE PROCEDURE get_id_dania_by_nazwa(IN p_nazwa varchar(256), OUT p_id_dania INT(10))
+ BEGIN
+ SELECT id_dania INTO p_id_dania FROM danie WHERE nazwa_dania LIKE p_nazwa;
+
+ END; $$
+
+ DROP PROCEDURE IF EXISTS get_id_definicji_skladnika_by_nazwa;
+ CREATE PROCEDURE get_id_definicji_skladnika_by_nazwa(IN p_nazwa varchar(256), OUT p_id_definicji INT(10))
+ BEGIN
+ SELECT id_definicja INTO p_id_definicji FROM definicje_produktow WHERE nazwa LIKE p_nazwa;
+ END; $$
+
+
+ 
+  DROP PROCEDURE IF EXISTS add_produkt_to_danie;
+  CREATE PROCEDURE add_produkt_to_danie(
+     IN p_nazwa_dania varchar(256), 
+     IN p_nazwa_skladnika varchar(256), 
+     IN p_ilosc float
+ )
+ BEGIN
+ DECLARE v_id_dania int(10);
+ DECLARE v_id_definicji_skladnika int(10);
+ 
+  DECLARE input VARCHAR(256);
+  SET input = `p_nazwa_dania`;
+  CALL get_id_dania_by_nazwa(input, v_id_dania);
+  SET input = p_nazwa_skladnika;
+  CALL get_id_definicji_skladnika_by_nazwa(input, v_id_definicji_skladnika); 
+  INSERT INTO skladniki_dania (zuzywana_ilosc, id_definicji_produktu, danie_id) VALUES (p_ilosc, v_id_definicji_skladnika, v_id_dania);
+ 
+  END; $$
+ 
+ 
+  DROP PROCEDURE IF EXISTS add_danie_to_menu;
+  CREATE PROCEDURE add_danie_to_menu(
+     IN nazwa_dania varchar(256), 
+     IN p_cena float
+ )
+ BEGIN
+ DECLARE v_id_dania int(10);
+ 
+  DECLARE input VARCHAR(256);
+  SET input := nazwa_dania;
+  CALL get_id_dania_by_nazwa(input, v_id_dania);
+  INSERT INTO menu (cena, id_dania) VALUES (p_cena, v_id_dania);
+ 
+  END; $$
+ 
+-- DELIMITER ;
+
+-- CALL insert_definicja_produktu("kg", "sól");
+-- CALL insert_danie("chleb i sól");
+-- CALL add_produkt_to_danie('chleb i sól', 'sól', 0.01);
+-- CALL add_danie_to_menu("chleb i sól", 30.5);
+CALL get_menu();
+-- CALL getAll();
