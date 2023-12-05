db2dfd44fdafcecf4047547429b41d5999efa859
Kacper Filipiak <kisielWrosole@gmail.com>
2023-12-06 00:18:36 +0100

    Added majority of procedures

--- database_queries/procedures.sql
+++ database_queries/procedures.sql
@@ -70,6 +70,113 @@ END; $$
  
   END; $$
   
+  DROP PROCEDURE IF EXISTS wyswietl_zamowienia;
+  CREATE PROCEDURE wyswietl_zamowienia()
+  BEGIN
+    SELECT * FROM zamowienia 
+    INNER JOIN zamowienia_danie ON zamowienia_danie.zamowienia_id = zamowienia.id_zamowienia 
+    INNER JOIN danie ON danie.id_dania = zamowienia_danie.danie_id 
+    WHERE zamowienia.status_zamowienia = 1;
+  END; $$
+   
+  DROP PROCEDURE IF EXISTS ustaw_status_zamowienia;
+  CREATE PROCEDURE ustaw_status_do_wydania(IN p_id_zamowienia INT(10), IN p_status INT(3))
+  BEGIN
+    UPDATE zamowienia SET status_zamowienia = p_status WHERE id_zmowienia = p_id_zamowienia;
+  END; $$
+   
+
+  DROP PROCEDURE IF EXISTS ustaw_status_do_wydania;
+  CREATE PROCEDURE ustaw_status_do_wydania(IN p_id_zamowienia INT(10))
+  BEGIN
+   ustaw_status_zamowienia(p_id_zamowienia, 2); 
+  END; $$
+ 
+  DROP PROCEDURE IF EXISTS ustaw_status_wydane;
+  CREATE PROCEDURE ustaw_status_wydane(IN p_id_zamowienia INT(10))
+  BEGIN
+   ustaw_status_zamowienia(p_id_zamowienia, 3); 
+  END; $$
+  
+  DROP PROCEDURE IF EXISTS utworz_zamowienie;
+  CREATE PROCEDURE utworz_zamowienie(IN p_id_stolika INT(10))
+  BEGIN
+    INSERT INTO zamowienia (id_stolika) VALUES (p_id_stolika);
+    SELECT LAST_INSERT_ID();
+  END; $$
+
+  DROP PROCEDURE IF EXISTS dodaj_danie_do_zamowienia;
+  CREATE PROCEDURE utworz_zamowienie(IN p_id_zamowienia INT(10), IN nazwa_dania VARCHAR(256))
+  BEGIN
+  DECLARE v_id_dania int(10);
+ 
+  DECLARE input VARCHAR(256);
+  SET input = `p_nazwa_dania`;
+  CALL get_id_dania_by_nazwa(input, v_id_dania);
+  INSERT INTO zamowienia_danie (id_zamowienia, id_dania) VALUES (p_id_zamowienia, v_id_dania);
+  END; $$
+
+  DROP PROCEDURE IF EXISTS sprawdz_dostepnosc_skladnika;
+  CREATE PROCEDURE sprawdz_dostepnosc_skladnika(IN p_nazwa_skladnika VARCHAR(256), IN p_ilosc INT(10))
+  BEGIN
+    SELECT IF(SUM(ilosc)>p_ilosc,TRUE,FALSE) FROM produkty INNER JOIN definicje_produktow ON typ = id_definicja WHERE nazwa LIKE p_nazwa_skladnika GROUP BY id_definicja;
+  END; $$
+
+ 
+  DROP PROCEDURE IF EXISTS wygeneruj_rachunek;
+  CREATE PROCEDURE wygeneruj_rachunek(IN p_id_zamowienia INT(10))
+  BEGIN
+    SELECT SUM(cena) FROM menu INNER JOIN zamowienia_danie ON danie_id = id_dania WHERE zamowienia_id = p_id_zamowienia;
+  END; $$
+ 
+  DROP PROCEDURE IF EXISTS ustaw_status_stolika;
+  CREATE PROCEDURE ustaw_status_stolika(IN p_id_stolika INT(10), IN p_status INT(2))
+  BEGIN
+    UPDATE stoliki SET status_stolika = p_status WHERE id_stolika = p_id_stolika;
+  END; $$
+ 
+  DROP PROCEDURE IF EXISTS zwolnij_stolik;
+  CREATE PROCEDURE zwolnij_stolik(IN p_id_stolika INT(10))
+  BEGIN
+    CALL ustaw_status_stolika(p_id_stolika);
+  END; $$
+ 
+  DROP PROCEDURE IF EXISTS zajmij_stolik;
+  CREATE PROCEDURE zajmij_stolik(IN p_id_stolika INT(10))
+  BEGIN
+    CALL ustaw_status_stolika(p_id_stolika);
+  END; $$
+
+  DROP PROCEDURE IF EXISTS dodaj_produkt;
+  CREATE PROCEDURE dodaj_produkt(IN p_nazwa VARCHAR(256), IN p_ilosc FLOAT, IN p_data_waznosci DATE)
+  BEGIN
+  DECLARE v_id_definicji_skladnika int(10);
+  DECLARE input VARCHAR(256);
+  SET input = p_nazwa_skladnika;
+  CALL get_id_definicji_skladnika_by_nazwa(input, v_id_definicji_skladnika); 
+  INSERT INTO produkty (ilosc, typ, data_waznosci) VALUES (p_ilosc, v_id_definicji_skladnika, p_data_waznosci);
+  END; $$
+
+  DROP PROCEDURE IF EXISTS get_skladniki_dania;
+  CREATE PROCEDURE get_skladniki_dania(IN p_nazwa VARCHAR(256))
+  BEGIN
+  DECLARE v_id_dania int(10);
+  DECLARE input VARCHAR(256);
+  SET input = p_nazwa;
+  CALL get_id_dania_by_nazwa(input, v_id_dania); 
+  SELECT definicje_produktow.nazwa, zuzywana_ilosc FROM definicje_produktow 
+  INNER JOIN skladniki_dania ON id_definicja = id_definicji_produktu 
+  INNER JOIN danie.id_dania = v_id_dania;
+  END; $$
+
+
+
+
+
+
+ 
+
+    
   DROP PROCEDURE IF EXISTS add_kelner;
   CREATE PROCEDURE add_kelner(
      IN p_login varchar(256), 
@@ -88,7 +195,7 @@ END; $$
     DEALLOCATE PREPARE `stmt`;
   FLUSH PRIVILEGES;
   END; $$
- 
+   
 -- DELIMITER ;
 
 -- CALL insert_definicja_produktu("kg", "sól");
