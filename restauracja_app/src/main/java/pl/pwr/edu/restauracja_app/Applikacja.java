da6ecd4eaf3b3e8483284e146ad83d2a64f1ff11
Kacper Filipiak <kisielWrosole@gmail.com>
2024-01-17 18:28:05 +0100

    Added navigation to next screens

--- restauracja_app/src/main/java/pl/pwr/edu/restauracja_app/Applikacja.java
+++ restauracja_app/src/main/java/pl/pwr/edu/restauracja_app/Applikacja.java
@@ -13,18 +13,25 @@ import java.io.IOException;
 
 public class Applikacja extends Application {
 
-	private LoginPresenter loginPresenter;
-	private AdminPresenter adminPresenter = new AdminPresenter();
+	private NavHelper navHelper;
+	private LoginPresenter loginPresenter = new LoginPresenter();
+	private AdminPresenter adminPresenter;
 	private KelnerPresenter kelnerPresenter;
 	private KucharzPresenter kucharzPresenter;
 	@Override
 	public void start(Stage stage) throws IOException {
-		FXMLLoader fxmlLoader = new FXMLLoader(Applikacja.class.getResource("admin-menu-view.fxml"));
-		fxmlLoader.setController(adminPresenter);
+		FXMLLoader fxmlLoader = new FXMLLoader(Applikacja.class.getResource("login-view.fxml"));
+		fxmlLoader.setController(loginPresenter);
 		Scene scene = new Scene(fxmlLoader.load(), 320, 240);
 		scene.getStylesheets().add("pl/pwr/edu/restauracja_app/style.css");
-		stage.setTitle("Admin menu");
+		stage.setTitle("Restauracja");
 		stage.setScene(scene);
+		navHelper = new NavHelper(scene);
+		adminPresenter = new AdminPresenter(navHelper);
+		navHelper.addScreen("login-view", "login-view.fxml", loginPresenter);
+		navHelper.addScreen("admin-menu-view", "admin-menu-view.fxml", adminPresenter);
+		navHelper.addScreen("dodaj-definicje-skladnika-view", "dodaj-definicje-skladnika-view.fxml", adminPresenter);
+		navHelper.navigateTo("admin-menu-view");
 		stage.show();
 	}
 
